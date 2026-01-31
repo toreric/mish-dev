c22073f84cada00591e66df238591f3b85ae2f5e
toreric <tore.ericsson@tores.se>
2026-01-31 19:26:03 +0100

    intermediate
    reverting to en-us etc

--- mish-project/app/routes/application.js
+++ mish-project/app/routes/application.js
@@ -10,8 +10,7 @@ export default class ApplicationRoute extends Route {
   async beforeModel() {
     // this.intl.setLocale(['sv-se']);
     // this.intl.setLocale(['de-de']);
-    // await this.intl.loadAndSetLocale();  // not a function
-    await this.intl.setLocale(['en-us']);
+    this.intl.setLocale(['en-us']);
     this.z.initialize();
   }
 }
